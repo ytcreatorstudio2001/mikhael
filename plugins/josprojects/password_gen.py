76ccf29569a57c1c27f7ef2bc1c82eab1aac5fd6
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-03-01 16:03:27 +0530

    Update password_gen.py

--- plugins/josprojects/password_gen.py
+++ plugins/josprojects/password_gen.py
@@ -31,6 +31,6 @@ async def password(bot, update):
         text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
     else:
         random_value = "".join(random.sample(password, limit))
-        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @JOSPSupport",
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]])
+        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @MovieHub_2k22",
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://t.me/MovieHub_2k22')]])
     await message.edit_text(text, True)
