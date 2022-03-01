d3d198a67ba65e2989238320eb9fe899a83cd558
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-03-01 16:05:15 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='join projects channel', url='https://t.me/MovieHub_2k22')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\nmade by @MovieHub_2k22 | @MovieHub_2k22 🔥"
         return shorten_urls
     except Exception as error:
         return error
