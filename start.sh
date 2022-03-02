9afda87c6b911e56472a6eff61708389cade26de
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-03-02 08:51:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/ytcreatorstudio2001/mikhael.git /mikhael
+cd /mikhael
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
